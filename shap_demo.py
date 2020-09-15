4e5995737c8f483387d59f0273065e532a76ae02
harupy <17039389+harupy@users.noreply.github.com>
2020-09-15 21:56:12 +0900

    commit
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- shap_demo.py
+++ shap_demo.py
@@ -1,5 +1,7 @@
 import logging
 
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.datasets import load_iris
 import shap
@@ -10,13 +12,11 @@ logging.getLogger("shap").setLevel(logging.DEBUG)
 
 
 def main():
-    import pandas as pd
-
-    # train a model
+    # train a multi-class classifier using the iris dataset
     X, y = load_iris(return_X_y=True, as_frame=True)
 
-    X = pd.concat((X,) * 1_0)
-    y = pd.concat((y,) * 1_0)
+    # X = pd.concat((X,) * 1_0)
+    # y = pd.concat((y,) * 1_0)
 
     print(X.shape)
     print(y.shape)
@@ -32,17 +32,33 @@ def main():
     print(expl.expected_value.shape)
     print(expl.shap_values.shape)
 
-    # create force plots using the loaded expalanation data and log them
+    # log some plots using logged explanation data
     with mlflow.start_run(run_id=run.info.run_id):
-        # create force plots
-        force_plot_single = shap.force_plot(
-            expl.expected_value[0], expl.shap_values[0][0, :], X.iloc[0, :]
-        )
-        force_plot_all = shap.force_plot(expl.expected_value[0], expl.shap_values[0], X)
+        label_index_to_plot = 0
 
-        # log force plots
-        mlflow.shap._log_force_plot(force_plot_single, "force_plot_single.html")
+        # force plot
+        force_plot_single0 = shap.force_plot(
+            expl.expected_value[label_index_to_plot],
+            expl.shap_values[label_index_to_plot][0, :],
+            X.iloc[0, :],
+        )
+        force_plot_single1 = shap.force_plot(
+            expl.expected_value[label_index_to_plot],
+            expl.shap_values[label_index_to_plot][1, :],
+            X.iloc[1, :],
+        )
+        force_plot_all = shap.force_plot(
+            expl.expected_value[label_index_to_plot], expl.shap_values[label_index_to_plot], X
+        )
+        mlflow.shap._log_force_plot(force_plot_single0, "force_plot_single0.html")
+        mlflow.shap._log_force_plot(force_plot_single1, "force_plot_single1.html")
         mlflow.shap._log_force_plot(force_plot_all, "force_plot_all.html")
+
+        # summary plot
+        shap.summary_plot(expl.shap_values[label_index_to_plot], X, show=False)
+        summary_plot = plt.gcf()
+        summary_plot.tight_layout()
+        mlflow.shap._log_matplotlib_figure(summary_plot, "summary_plot.png")
 
 
 if __name__ == "__main__":
