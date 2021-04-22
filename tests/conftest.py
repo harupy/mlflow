75187a81211d2b7cd9241672e305984cae3ea607
harupy <17039389+harupy@users.noreply.github.com>
2021-04-22 09:57:52 +0900

    updat
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -66,8 +66,8 @@ def clean_up_leaked_runs():
         ALL_TESTS_BEFORE.append(test_info)
         yield
         ALL_TESTS_AFTER.append(test_info)
-        if not mlflow.active_run():
-            leakd_run = mlflow.get_run(mlflow.active_run.info.run_id)
+        if mlflow.active_run():
+            leakd_run = mlflow.get_run(mlflow.active_run().info.run_id)
             print(leakd_run)
             msg = (
                 "test case unexpectedly leaked a run!: "
