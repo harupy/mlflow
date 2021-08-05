8d33e6d6e1c44822084c7578f98c367f704b5dd3
harupy <hkawamura0130@gmail.com>
2021-08-05 14:15:56 +0900

    fix
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/notify-dco-failure.js
+++ .github/workflows/notify-dco-failure.js
@@ -32,10 +32,7 @@ module.exports = async ({ context, github }) => {
 
   const dcoCheck = await getDcoCheck();
   if (dcoCheck.conclusion === "success") {
-    const body = [
-      `@${user.login} The DCO check failed. Please sign off your commits:`,
-      `${dcoCheck.html_url}`,
-    ].join("\n");
+    const body = `@${user.login} The DCO check failed. Please sign off your commits: ${dcoCheck.html_url}`;
     await github.issues.createComment({
       owner,
       repo,
