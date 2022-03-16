10a606e85b8d0427684869d146d619d6bde80d92
harupy <17039389+harupy@users.noreply.github.com>
2022-03-16 18:40:13 +0900

    f
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- a.py
+++ a.py
@@ -1,7 +1,9 @@
 import subprocess
 
-p = subprocess.Popen(["pip", "install", '"requirements\\lint-requirements.txt"'], shell=True)
+p = subprocess.Popen(["cmd", "/c", 'pip install "requirements\\lint-requirements.txt"'], shell=True)
 p.wait()
 
-p = subprocess.Popen(["pip", "install", '"requirements\\lint-requirements.txt"'], shell=False)
+p = subprocess.Popen(
+    ["cmd", "/c", 'pip install "requirements\\lint-requirements.txt"'], shell=False
+)
 p.wait()
