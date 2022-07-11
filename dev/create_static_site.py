0e441b3f13e3b1d4c9409824d0ddea90b5352426
harupy <hkawamura0130@gmail.com>
2022-07-11 16:09:40 +0900

    test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ dev/create_static_site.py
@@ -0,0 +1,52 @@
+import argparse
+import requests
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--render-token", required=True)
+    parser.add_argument("--render-owner-id", required=True)
+    parser.add_argument("--repo", required=True)
+    parser.add_argument("--branch", required=True)
+    parser.add_argument("--pull-number", required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    url = "https://api.render.com/v1/services"
+    payload = {
+        "autoDeploy": "yes",
+        "serviceDetails": {
+            "publishPath": "mlflow/server/js/build",
+            "pullRequestPreviewsEnabled": "no",
+            "routes": [
+                {"type": "redirect", "source": "/static-files/*", "destination": "/*"},
+                {
+                    "type": "rewrite",
+                    "source": "/ajax-api/*",
+                    "destination": "https://mlflow.onrender.com/ajax-api/*",
+                },
+                {
+                    "type": "rewrite",
+                    "source": "/get-artifact",
+                    "destination": "https://mlflow.onrender.com/get-artifact",
+                },
+            ],
+            "buildCommand": "cd mlflow/server/js && yarn install && yarn build",
+        },
+        "ownerId": args.owner_id,
+        "type": "static_site",
+        "name": f"mlflow-pr-{args.pull_number}",
+        "repo": f"https://github.com/{args.repo}",
+        "branch": args.pull_number,
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {args.token}",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
