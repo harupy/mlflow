f7748654d216398c3faa403a85dd73188120f0d5
harupy <hkawamura0130@gmail.com>
2023-06-08 12:47:36 +0900

    cancel
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- .github/workflows/cancel.js
+++ .github/workflows/cancel.js
@@ -5,22 +5,25 @@ module.exports = async ({ context, github }) => {
   const headSha = context.payload.pull_request.head.sha;
 
   // Get all workflow runs associated with the PR.
-  const prRuns = await github.rest.actions.listWorkflowRunsForRepo({
+  const prRuns = await github.paginate(github.rest.actions.listWorkflowRunsForRepo, {
     owner,
     repo,
     head_sha: headSha,
     event: "pull_request",
-    status: "in_progress",
     per_page: 100,
   });
-  console.log(prRuns.data.workflow_runs);
 
   // Cancel the runs
   for (const run of prRuns.data.workflow_runs) {
-    await github.rest.actions.cancelWorkflowRun({
-      owner,
-      repo,
-      run_id: run.id,
-    });
+    try {
+      await github.rest.actions.cancelWorkflowRun({
+        owner,
+        repo,
+        run_id: run.id,
+      });
+      console.log(`Cancelled run ${run.id}`);
+    } catch (error) {
+      console.error(`Failed to cancel run ${run.id}`, error);
+    }
   }
 };
