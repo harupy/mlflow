0825a3514ab04154ee3982ec918150d055a8dfd0
harupy <17039389+harupy@users.noreply.github.com>
2024-09-03 16:38:20 +0900

    wip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

---
+++ .github/workflows/re-run.js
@@ -0,0 +1,44 @@
+module.exports = async ({ github, context }) => {
+  const {
+    repo: { owner, repo },
+  } = context;
+
+  const { data: pr } = await github.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: context.issue.number,
+  });
+
+  const checkRuns = await github.paginate(github.rest.checks.listForRef, {
+    owner,
+    repo,
+    ref: pr.head.sha,
+  });
+  const runIdsToRerun = checkRuns
+    // Select failed github actions runs
+    .filter(
+      ({ name, status, conclusion, app: { slug } }) =>
+        slug === "github-actions" &&
+        status === "completed" &&
+        conclusion === "failure" &&
+        name !== "re-run"
+    )
+    .map(
+      ({
+        // Example: https://github.com/mlflow/mlflow/actions/runs/10675586265/job/29587793829
+        //                                                        ^^^^^^^^^^^ run_id
+        html_url,
+      }) => html_url.match(/\/actions\/runs\/(\d+)/)[1]
+    );
+
+  // Re-run failed github actions runs
+  await Promise.all(
+    [...new Set(runIdsToRerun)].map((run_id) =>
+      github.rest.actions.reRunWorkflowFailedJobs({
+        repo,
+        owner,
+        run_id,
+      })
+    )
+  );
+};
