161d737e494a04fc2d46d26cd3b35305bab07ec8
harupy <17039389+harupy@users.noreply.github.com>
2025-02-28 12:25:47 +0900

    wip
    Signed-off-by: harupy <17039389+harupy@users.noreply.github.com>

--- mlflow/utils/request_utils.py
+++ mlflow/utils/request_utils.py
@@ -73,14 +73,14 @@ def augmented_raise_for_status(response):
 def download_chunk(*, range_start, range_end, headers, download_path, http_uri):
     combined_headers = {**headers, "Range": f"bytes={range_start}-{range_end}"}
 
+    print("Downloading chunk:", http_uri, headers)
     with cloud_storage_http_request(
         "get",
         http_uri,
         stream=False,
         headers=combined_headers,
-        timeout=10,
+        timeout=30,
     ) as response:
-        print("Downloading chunk:", http_uri, headers)
         expected_length = response.headers.get("Content-Length")
         if expected_length is not None:
             actual_length = response.raw.tell()
